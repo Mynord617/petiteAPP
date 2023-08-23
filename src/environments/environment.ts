c19d3109b360a239a4bb0091f90cd920b93656b7
Estefany2265 <131624575+Estefany2265@users.noreply.github.com>
2023-08-23 01:11:18 -0600

    Implementacion firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'petiteapp-1936f',
+    appId: '1:456324153267:web:7e7c9399d8530d91d315c6',
+    databaseURL: 'https://petiteapp-1936f-default-rtdb.firebaseio.com',
+    storageBucket: 'petiteapp-1936f.appspot.com',
+    apiKey: 'AIzaSyCEChNOlp0jH-HhxTKolrmeZCFwgDJh6OQ',
+    authDomain: 'petiteapp-1936f.firebaseapp.com',
+    messagingSenderId: '456324153267',
+    measurementId: 'G-DKKV5ZTWCQ',
+  },};
