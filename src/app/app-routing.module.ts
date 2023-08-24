76102f90062710f9ca9f2dbb6975241b07dac8cb
AllanFl0res <the_Darkplayer@outlook.es>
2023-08-23 18:05:31 -0600

    Creacion de componente login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+{ path: 'registro', loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroModule) },
+{ path:'**',pathMatch:'full',redirectTo:'/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
